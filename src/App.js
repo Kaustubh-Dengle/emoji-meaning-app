bbe331a4fc8e0834f4aa9109d0ddee8d8b81854f
Kaustubh Dengle <54681692+Kaustubh-Dengle@users.noreply.github.com>
2020-12-08 16:26:43 +0530

    inputhandling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  function likeClickHandler() {
-    console.log("clicked");
+  const [userInput, setUserInput] = useState("");
+  function onChangeHandler(event) {
+    setUserInput(event.target.value);
   }
+
   return (
     <div className="App">
       <h1>welcome</h1>
-      <button onClick={likeClickHandler}>click me</button>
+      <input onChange={onChangeHandler}></input>
+      <div> hello {userInput}</div>
     </div>
   );
 }
